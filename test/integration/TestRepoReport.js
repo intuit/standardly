8fad903072d07ded132a5f50cf98499fcbeaae85
rmontes <rocio_montes@intuit.com>
2019-03-21 15:01:38 -0700

    tests draft

--- test/integration/TestRepoReport.js
+++ test/integration/TestRepoReport.js
@@ -1,26 +1,24 @@
 const chai = require("chai");
 const expect = chai.expect;
-const gitHubUtils = require("../../src/lib/gitHubUtils.js");
-const path = require("path");
 const shelljs = require("shelljs");
-const common = require("../../src/lib/common");
-const appPath = common.appPath;
-const tmpDir = path.join(appPath, "/tmp");
 const fse = require("fs-extra");
 
 describe("Tool report is consistent", function() {
-    it("Report output is consistent for standarly", () => {
-        return fse.emptyDir(tmpDir).then(() => {
-            gitHubUtils.cloneGitRepo("https://github.com/intuit/saloon.git", tmpDir)
-                .then((response) => {
-                    expect(response).to.be.not.empty;
-                    expect(path.basename(response, ".git")).to.eql("saloon");
+    it("Report output is consistent", () => {
+        const toolcmd= "node src/app.js --giturl https://github.com/intuit/saloon.git  -r sample/rules.json"
+        shelljs.exec(toolcmd, {silent:true}, ()=> {
+           var sampleResult;
+            return fse.readFile('test/resources/sample_results.csv')
+                .then((sample)=>{
+                    sampleResult = sample;
+                    return(fse.readFile('reports/results.csv'))
                 })
-                .finally(() => {
-                    shelljs.rm("-rf", tmpDir);
-                });
-        });
+                .then((actualResult)=>{ 
+                    var comparison = Buffer.compare(sampleResult, actualResult);
+                    console.log(comparison);
+                    return expect(comparison).to.eql(0);
+                })
+                .catch((err)=>{throw err;})
+          });   
     });
-    
-
 });
